2b8d4ed63215f12ccbff2b003f520ec46351ee7b
fdc1994 <fdc1994@hotmail.com>
2024-03-30 17:58:59 +0100

    test

--- app/src/main/java/com/example/trueweather/MainActivity.kt
+++ app/src/main/java/com/example/trueweather/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.main_text_view)
-        textView.text = "this is a test. test"
+        textView.text = "this is a test"
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
