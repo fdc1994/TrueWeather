6c21ead3085e76c6ea6e0d03d78dc41719f1733c
fdc1994 <fdc1994@hotmail.com>
2024-04-11 11:51:26 +0200

    make API call in rough activity

--- app/src/main/java/com/example/trueweather/MainActivity.kt
+++ app/src/main/java/com/example/trueweather/MainActivity.kt
@@ -3,35 +3,52 @@ package com.example.trueweather
 import android.content.Context
 import android.os.Bundle
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.domain.data.WeatherForecast
 import com.example.domain.data.managers.IpmaNetworkManager
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.android.schedulers.AndroidSchedulers
 import javax.inject.Inject
 
 @AndroidEntryPoint
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var ipmaRepository: IpmaNetworkManager
 
     private lateinit var textView: TextView
+    private lateinit var refreshButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.main_text_view)
+        refreshButton = findViewById(R.id.refresh_button)
 
-        ipmaRepository.getWeatherForecast("3434").subscribe { success ->
+        refreshButton.setOnClickListener {
+            ipmaRepository.getWeatherForecast("1010500")
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { (_, _, data): WeatherForecast ->
+                        textView.text = data.toString()
+                    }
+                )
+                { throwable: Throwable ->
+                    textView.text = throwable.message
 
-            textView.text = success.data.count().toString()
+                    Log.e("TAG", "Error: ", throwable)
+                }
         }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- network/src/main/java/com/example/network/interfaces/IPMAService.kt
+++ network/src/main/java/com/example/network/interfaces/IPMAService.kt
@@ -1,13 +1,11 @@
 package com.example.network.interfaces
 
-import com.example.network.data.WeatherDataDTO
 import com.example.network.data.WeatherForecastDTO
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
-import javax.inject.Inject
 
 interface IPMAService {
-    @GET("forecast/meteorology/cities/daily/{globalIdLocal}.json")
+    @GET("open-data/forecast/meteorology/cities/daily/{globalIdLocal}.json")
     fun getWeatherData(@Path("globalIdLocal") globalIdLocal: String): Single<WeatherForecastDTO>
 }
