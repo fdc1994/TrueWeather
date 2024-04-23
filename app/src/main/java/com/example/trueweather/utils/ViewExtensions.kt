c4bfd27397b18bc8aaf19895c5ac2a3fff7ce6a6
fdc1994 <fdc1994@hotmail.com>
2024-04-23 15:50:32 +0200

    add view extensions

--- app/src/main/java/com/example/trueweather/utils/ViewExtensions.kt
+++ app/src/main/java/com/example/trueweather/utils/ViewExtensions.kt
@@ -4,8 +4,13 @@ import android.app.Activity
 import android.view.View
 import androidx.annotation.IdRes
 import androidx.annotation.UiThread
+import androidx.core.view.isGone
 
 @UiThread
 fun <V : View?> Activity.lazyFindViewById(@IdRes id: Int) =
     lazy(LazyThreadSafetyMode.NONE) { findViewById<V>(id) }
+
+fun View.setGone(isGone: Boolean) {
+    this.isGone = isGone
+}
 
