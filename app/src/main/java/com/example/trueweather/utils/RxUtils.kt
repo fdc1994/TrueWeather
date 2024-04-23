ca04ccc229d1cc9cb3e8b8353ed70c6007f15e7d
fdc1994 <fdc1994@hotmail.com>
2024-04-23 15:49:21 +0200

    improve RxUtils.kt

--- app/src/main/java/com/example/trueweather/utils/RxUtils.kt
+++ app/src/main/java/com/example/trueweather/utils/RxUtils.kt
@@ -3,6 +3,13 @@ package com.example.trueweather.utils
 sealed class RxResult<out T> {
     data class Success<out T>(val data: T) : RxResult<T>()
     data class Error(val errorType: ErrorType) : RxResult<Nothing>()
+
+    fun getValueOrNull(): T? {
+        return when (this) {
+            is Success -> data
+            is Error -> null
+        }
+    }
 }
 
 enum class ErrorType {
